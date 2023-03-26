57702288f52080da34fe82d9bf5febf711f18ae1
UlianaMeashkova <el.mahno@gmail.com>
2023-03-26 13:34:44 +0300

    tests

--- shop/products/tests.py
+++ shop/products/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+def test_products_index():
+    response = requests.get("http://127.0.0.1:8000")
+    assert response.status_code == 200
+    assert "Product:" in str(response.content)
--- shop/profiles/tests.py
+++ shop/profiles/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+def test_profiles():
+    response = requests.get("http://127.0.0.1:8000/profiles")
+    assert response.status_code == 200
+    assert "Profiles view" in str(response.content)
