0dd56b02b96e08fc1fab0b10f24b90611f73e071
UlianaMeashkova <el.mahno@gmail.com>
2023-03-27 18:55:53 +0300

    login done

--- shop/tests/test_login.py
+++ shop/tests/test_login.py
@@ -3,4 +3,4 @@ import requests
 def test_login():
     response = requests.get("http://127.0.0.1:8000/login")
     assert response.status_code == 200
-    assert "" in str(response.content)
+    assert "Login user" in str(response.content)
