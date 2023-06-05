d2527c0454c06dd66c8c738dddf1e4d866070a30
UlianaMeashkova <el.mahno@gmail.com>
2023-06-05 19:35:20 +0300

    img

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ROOT_DIR = BASE_DIR.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -171,10 +171,10 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = None
+STATIC_ROOT = [ROOT_DIR/"static"]
 STATIC_URL = "static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = ROOT_DIR / "media"
 MEDIA_URL = "media/"
 
 # Default primary key field type
